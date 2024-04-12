bf67275e150090b685d190e6c11c7b96c6def39b
venkateshps321 <163814152+venkateshps321@users.noreply.github.com>
2024-04-12 13:56:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Sam Wilson teams up with Bucky Barnes on a global mission to stop an anti-patriotism group, the Flag Smashers, who are enhanced with a recreation of the Super Soldier Serum and believe the world!',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des:'The first season of the American television series Loki, based on Marvel Comics featuring the character of the same name, sees Loki brought to the mysterious Time Variance Authority (TVA) after stealing the Tesseract during the events of Avengers: Endgame (2019)!',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity.!',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides!',
+        image: 'images/slider 5.PNG'
+    },
+    
+    {
+        name:'avengers',
+        des: 'Newlywed couple Wanda Maximoff and Vision move into the town of Westview in a black-and-white 1950s setting. They attempt to blend in despite Vision being an android and Wanda having telekinesis and reality-warping abilities!',
+        image:'images/slider 3.PNG'
+    }
+]
